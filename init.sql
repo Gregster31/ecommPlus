3dc84ecad7786a30a0435a5bd6cb3b62b02d24c2
Julien <julienhalde9@gmail.com>
2024-05-13 01:00:50 +0000

    Changed DB, correctly creates all tables.

--- init.sql
+++ init.sql
@@ -6,6 +6,23 @@ CREATE DATABASE "ECommDB";
 DROP TYPE IF EXISTS order_status;
 CREATE TYPE order_status AS ENUM ('incomplete', 'complete');
 
+DROP TABLE IF EXISTS "category";
+CREATE TABLE "category" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(50) NOT NULL
+);
+
+DROP TABLE IF EXISTS "product";
+CREATE TABLE "product" (
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR(50) NOT NULL,
+    "description" VARCHAR(100) NOT NULL,
+    "date" TIMESTAMP NOT NULL,
+    "price" DECIMAL NOT NULL,
+    "inventory" INTEGER NOT NULL,
+    "category_id" INTEGER REFERENCES "category"(id) ON DELETE CASCADE
+);
+
 DROP TABLE IF EXISTS "customer";
 CREATE TABLE "customer" (
     "id" SERIAL PRIMARY KEY,
@@ -17,17 +34,6 @@ CREATE TABLE "customer" (
     "password" Text NOT NULL,
     "user_name" VARCHAR(20) NOT NULL,
     "is_admin" BOOLEAN DEFAULT FALSE
-);
-
-DROP TABLE IF EXISTS "order";
-CREATE TABLE "order" (
-    "id" SERIAL PRIMARY KEY,
-    "order_date" TIMESTAMP NOT NULL,
-    "total_price" DECIMAL NOT NULL,
-    status order_status NOT NULL DEFAULT 'incomplete',
-    completed_at TIMESTAMP,
-    "customer_id" INTEGER REFERENCES customer(id) ON DELETE CASCADE,
-    "address_id" INTEGER REFERENCES address(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS "address";
@@ -43,29 +49,25 @@ CREATE TABLE "address" (
     "customer_id" INTEGER REFERENCES customer(id) ON DELETE CASCADE
 );
 
+DROP TABLE IF EXISTS "order";
+CREATE TABLE "order" (
+    "id" SERIAL PRIMARY KEY,
+    "order_date" TIMESTAMP NOT NULL,
+    "total_price" DECIMAL NOT NULL,
+    status order_status NOT NULL DEFAULT 'incomplete',
+    completed_at TIMESTAMP,
+    "customer_id" INTEGER REFERENCES customer(id) ON DELETE CASCADE,
+    "address_id" INTEGER REFERENCES address(id) ON DELETE CASCADE
+);
+
+
+
 DROP TABLE IF EXISTS "order_detail";
 CREATE TABLE "order_detail" (
     "order_id" INTEGER REFERENCES "order"(id) ON DELETE CASCADE,
     "product_id" INTEGER REFERENCES "product"(id) ON DELETE CASCADE,
     "unit_price" DECIMAL NOT NULL,
     PRIMARY KEY ("order_id", "product_id")
-);
-
-DROP TABLE IF EXISTS "product";
-CREATE TABLE "product" (
-    "id" SERIAL PRIMARY KEY,
-    "title" VARCHAR(50) NOT NULL,
-    "description" VARCHAR(100) NOT NULL,
-    "date" TIMESTAMP NOT NULL,
-    "price" DECIMAL NOT NULL,
-    "inventory" INTEGER NOT NULL,
-    "category_id" INTEGER REFERENCES "category"(id) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS "category";
-CREATE TABLE "category" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS "payment";
